c5bea4b5380cdfe68a43fb338c3580765bbfe145
Regil Koshy <regil_koshy@yahoo.com>
2022-06-14 15:44:22 -0500

    Updated git ignore file to ignore node modules

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,5 @@ class HomeController < ApplicationController
 
   def about
     @about_me = "My Name is Regil Koshy..."
-    @int1 = 50000
-    @int2 = 70000
-    @answer = @int1 + @int2
-    @xyz = 300
   end
 end
